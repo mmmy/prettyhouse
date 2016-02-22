c527869ba1b9c26c2251ed648b73dc7e624bf2c5
yangqi <531905408@qq.com>
2016-02-23 00:47:30 +0800

    urlencode

--- fetch_data.js
+++ fetch_data.js
@@ -1,7 +1,25 @@
 var fs = require('fs');
 var http = require('http');
+var urlencode = require('urlencode')
 var pkg = require('./package.json');
 
 var citys = process.argv.length > 2 ? process.argv.slice(2) : pkg.citys ;
 
 console.log(citys);
+
+var getCityAllData = function(cityName){
+	cityName = urlencode(cityName, 'utf8');
+	var url = "http://v.juhe.cn/estate/query?city=" + cityName + "&q=&page=&dtype=&key=44e487fceb1918419da1fe9a2c090f6b";
+	http.get(url, function(res){
+		res.on('data', (chunk) => {
+		    console.log(chunk.toString());
+		  });
+		  res.on('end', () => {
+		    console.log('No more data in response.')
+		  })
+	}).on('error', function(e){
+		console.error(e);
+	});
+};
+
+getCityAllData("深圳");
